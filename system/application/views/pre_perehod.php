7c9dcdf9722adcd2795c43c1f5b2fdaa2b58a130
keprom <kokshetau.energo.prom@mail.ru>
2018-12-27 17:28:44 +0600

    added perenos rekvizit schet

--- system/application/views/pre_perehod.php
+++ system/application/views/pre_perehod.php
@@ -1,11 +1,3 @@
-<?php //echo anchor('billing/perehod', "Перейти в след месяц"); ?><!--<br><br>-->
-<?php //echo anchor('billing/tariff_list', "Тарифы"); ?><!--<br><br>-->
-<?php //echo anchor('billing/dbase', "Импортировать оплату"); ?><!--<br><br>-->
-<?php //echo anchor('billing/pre_nachislenie_v_analiz', "Экспортировать начисление в анализ по тп"); ?><!--<br><br>-->
-<?php //echo anchor('billing/perenos_rek1', "Перенос реквизитов"); ?><!--<br><br>-->
-<?php //echo anchor('billing/perenos_nach', "Перенос начисления по уровням"); ?><!--<br><br>-->
-<?php //echo anchor('billing/perenos_saldo', "Сальдо за месяц"); ?><!--<br><br>-->
-<?php //echo anchor('billing/perenos_oplata', "Перенос оплаты за месяц"); ?>
 <?php
 $links = array(
     "billing/perehod" => array("title" => "Перейти в след месяц"),
@@ -13,7 +5,7 @@ $links = array(
     "billing/pre_nachislenie_v_analiz" => array("title" => "Экспортировать начисление в анализ по ТП"),
     "billing/nachislenie_v_buhgalteriu" => array("title" => "Перенос начисления в бухгалтерию"),
     "billing/perenos_rek1" => array("title" => "Перенос реквизитов"),
-    "billing/perenos_nach" => array("title" => "Перенос начисления в 1С 8.3"),
+    "billing/export_rekvizit_schet" => array("title" => "Перенос начисления в 1С 8.3"),
     "billing/tariff_list" => array("title" => "Тарифы"),
     "billing/perenos_saldo" => array("title" => "Сальдо за месяц"),
     "billing/perenos_oplata" => array("title" => "Перенос оплаты за месяц")
